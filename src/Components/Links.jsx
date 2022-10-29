08f2f88757af1dde9c5410e0563907b9756700d7
Bolu Tejumola <tejumolabolu@gmail.com>
2022-10-29 16:30:10 +0100

    create the profile section

---
+++ src/Components/Links.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from '../Image/bolu.png';
+const Links = () => {
+  return (
+    <div>
+        <div className="profile-section">
+        <img id="profile__img"src={Image} alt="" />
+        <h3 id="slack">Boluwatife Tejumola</h3>
+      </div>
+
+    </div>
+  )
+}
+
+export default Links
